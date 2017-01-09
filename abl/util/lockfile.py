3ddb11bc58b8e154aa3de24518e71be6d39168df
dir <dir@ableton.com>
2017-01-09 16:21:22 +0100

    Use correct error code on Windows for locked files

--- abl/util/lockfile.py
+++ abl/util/lockfile.py
@@ -60,7 +60,8 @@ class LockFile(object):
             else:
                 fcntl.flock(self.file, lock_flags)
         except IOError, e:
-            if e[0] == errno.EAGAIN:
+            error_code = errno.EACCES if is_windows else errno.EAGAIN
+            if e[0] == error_code:
                 raise LockFileObtainException()
             raise
 
